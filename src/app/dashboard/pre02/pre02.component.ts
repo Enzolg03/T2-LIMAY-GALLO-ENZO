836be953a9e52cfe533f95c76b9523528afdd5fc
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2024-06-22 21:11:28 -0500

    Pregunta 02 vista

--- src/app/dashboard/pre02/pre02.component.ts
+++ src/app/dashboard/pre02/pre02.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-pre02',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './pre02.component.html',
   styleUrl: './pre02.component.css'
 })
 export class Pre02Component {
-
+  
 }
