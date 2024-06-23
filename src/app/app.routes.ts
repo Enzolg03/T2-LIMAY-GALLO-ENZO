281bd07ab30ee788a00e63e3e80b7036f69d4f10
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2024-06-22 20:13:13 -0500

    agregando vista y rutas login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "login", component: LoginComponent},
+    { path: "dashboard", component: DashboardComponent},
+    { path: "", redirectTo: "login", pathMatch: "full" },
+];
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private router: Router){
+  }
 
 }
