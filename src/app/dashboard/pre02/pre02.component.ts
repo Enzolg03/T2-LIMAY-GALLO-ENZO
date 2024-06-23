a482ec8812b77a63033983fa76950eb80d3bbfae
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2024-06-22 21:12:36 -0500

    Prgeunta 02 logica

--- src/app/dashboard/pre02/pre02.component.ts
+++ src/app/dashboard/pre02/pre02.component.ts
@@ -9,5 +9,17 @@ import { MaterialModule } from '../../angular-material/material/material.module'
   styleUrl: './pre02.component.css'
 })
 export class Pre02Component {
-  
+  total : number;
+  calcularTotal(precio: HTMLInputElement, cantidad : HTMLInputElement){
+    let pre = Number(precio.value)
+    let cant = Number(cantidad.value)
+    let total =  pre * cant;
+    if(cant < 20){
+      total = total - (total* 0.1)
+    }else
+        if(cant >= 10){
+          total = total - (total * 0.05)
+        }
+    this.total = total
+  }
 }
