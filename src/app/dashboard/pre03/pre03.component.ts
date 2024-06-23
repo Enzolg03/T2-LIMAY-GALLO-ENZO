7a059f928ed28e77950cd57471c46db474dc7805
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2024-06-22 21:27:22 -0500

    Pregunta 03 - logica

--- src/app/dashboard/pre03/pre03.component.ts
+++ src/app/dashboard/pre03/pre03.component.ts
@@ -9,5 +9,17 @@ import { MaterialModule } from '../../angular-material/material/material.module'
   styleUrl: './pre03.component.css'
 })
 export class Pre03Component {
-  
+  total : number;
+  precio: number;
+  calcularTotal(cantidad: HTMLInputElement){
+    let cant = Number(cantidad.value)
+    let total: number;
+    if(cant < 5){
+      this.precio = 800
+    }else
+        if(cant >= 5){
+          this.precio = 700
+      }
+    this.total = total = cant * this.precio;
+  }
 }
