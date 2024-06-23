34097a2bdd0e549d0ace36c57e50428e291acc90
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2024-06-22 21:40:58 -0500

    Pregunta 04 - vista

--- src/app/dashboard/pre04/pre04.component.ts
+++ src/app/dashboard/pre04/pre04.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-pre04',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './pre04.component.html',
   styleUrl: './pre04.component.css'
 })
 export class Pre04Component {
-
+  
 }
