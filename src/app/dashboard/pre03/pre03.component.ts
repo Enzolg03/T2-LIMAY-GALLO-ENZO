1defb69d1dd952fd7710393b5637d498c3a6ea32
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2024-06-22 21:26:49 -0500

    Pregunta 03 - vista

--- src/app/dashboard/pre03/pre03.component.ts
+++ src/app/dashboard/pre03/pre03.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-pre03',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './pre03.component.html',
   styleUrl: './pre03.component.css'
 })
 export class Pre03Component {
-
+  
 }
